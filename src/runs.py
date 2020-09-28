b5494578606bfbfb0b0055cdba9a16bd1ac61b36
Felix Leeb <felix.leeb@tuebingen.mpg.de>
2020-09-28 12:49:15 +0200

    added foundation as related proj

---
+++ src/runs.py
@@ -0,0 +1,45 @@
+
+import omnifig as fig
+
+
+
+
+@fig.AutoModifier('cls-run')
+class OnCluster(Run):
+
+	def startup(self):
+
+		super().startup()
+
+		save_dir = self.save_dir
+
+		if save_dir is not None and 'JOBDIR' in os.environ:
+			jobdir = os.environ['JOBDIR']
+			cname = 'checkpoints{}.txt'.format(os.environ['PROCESS_ID'])
+
+			if cname not in os.listdir(jobdir):
+
+				# register job
+				if 'JOB_REGISTRY_PATH' in os.environ:
+					rpath = os.environ['JOB_REGISTRY_PATH']
+					reg = load_yaml(rpath) if os.path.isfile(rpath) else []
+					reg.append({
+						'timestamp': get_now(),
+
+						'id': os.environ['JOB_ID'].split('#')[-1],
+						'num': int(os.environ['JOB_NUM']),
+						'proc': int(os.environ['PROCESS_ID']),
+
+						'host': socket.gethostname(),
+
+						'run': save_dir,
+						'job': jobdir,
+					})
+					save_yaml(reg, rpath)
+
+				with open(os.path.join(jobdir, cname), 'w') as f:
+					f.write(os.path.basename(save_dir))
+				print('[Saved checkpoint dir for restarts]')
+
+
+
