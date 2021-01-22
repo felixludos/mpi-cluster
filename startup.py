e4ed636ceb8653b8c296092827b110f85242557b
felixludos <felix.leeb@hotmail.com>
2021-01-22 16:07:38 +0100

    fixes

--- startup.py
+++ startup.py
@@ -1,4 +1,9 @@
+print('startingup mpi-cluster')
+print(__file__)
+import src
+print(src.__file__)
 import submit_jobs
 import get_status
 import purge_jobs
 # import create_job
+print('done startuping mpi-cluster')
